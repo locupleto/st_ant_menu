3812787510aea4d1437c8cfa4fa3f52dec433829
Urban Ottosson <urban@ottosson.org>
2025-04-07 20:48:38 +0200

    Add built frontend files for st_ant_menu component

---
+++ st_ant_menu/frontend/build/precache-manifest.4770e53ba17ba42b9b71a40ffaadcdcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b384e5ebc78df0527ba6455d71b12558",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d6e43a8e34190fd899f",
+    "url": "./static/js/2.4038f065.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.4038f065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95956383f0663d85f7a5",
+    "url": "./static/js/main.74375ae0.chunk.js"
+  },
+  {
+    "revision": "23b81c6aad4ea3280af0",
+    "url": "./static/js/runtime-main.ac51b7c4.js"
+  }
+]);
